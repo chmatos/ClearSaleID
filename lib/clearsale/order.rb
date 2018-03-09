c8158c1768ecd0998edadf0e38fad515bb10bb18
Christiano Matos <chmatos@chmatos.com>
2018-03-09 08:49:15 -0300

    remove address required

--- lib/clearsale/order.rb
+++ lib/clearsale/order.rb
@@ -122,7 +122,7 @@ module Clearsale
         b.tag!('DataValidadeCartao', payment.card_expiration)
         b.tag!('NomeTitularCartao', payment.customer_name)
         b.tag!('DocumentoLegal1', user.cpf.present? ? user.cpf.gsub(/[\.\-]*/, '').strip : nil)
-        build_collection_address(b, order.billing_addre if order.billing_address.present?
+        build_collection_address(b, order.billing_address) if order.billing_address.present?
       end
     end
 
